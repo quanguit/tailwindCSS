905b973629d0b2210adbaba0e01a9a485b6d0b2d
Quang DN <quang.dn@clv.local>
2024-07-25 15:18:11 +0700

    Add lifestyle stories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,41 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      xs: "480px",
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
-        "coffee": {
+        coffee: {
           50: "#E8D6D0",
           200: "#C89F94",
           400: "#A25F4B",
-          600: "#744838"
-        }
+          600: "#744838",
+        },
       },
       fontFamily: {
         Karla: ["Karla", "sans-serif"],
       },
       keyframes: {
-         slideDown: {
+        slideDown: {
           "0%": { transform: "translateY(-100%)" },
           "100%": { transform: "translateY(0)" },
-         },
-         fadeIn: {
+        },
+        fadeIn: {
           from: { opacity: 0 },
           to: { opacity: 1 },
-         },
+        },
       },
       animation: {
         slideDown: "slideDown .5s ease-in-out",
         fadeIn: "fadeIn .5s ease-in-out",
       },
       backgroundImage: {
-        'slider-bg': "url('../public/img/slider.jpg')",
-      }
+        "slider-bg": "url('../public/img/slider.jpg')",
+      },
     },
   },
   plugins: [],
